a8a66b915c27cca89e14aad3fe0dc8cec1873982
Greg Farr <farr.gs@protonmail.com>
2022-10-26 12:08:56 +0200

    Initial commit

---
+++ tests/unit/test_jenkins_stack.py
@@ -0,0 +1,15 @@
+import aws_cdk as core
+import aws_cdk.assertions as assertions
+
+from jenkins.jenkins_stack import JenkinsStack
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in jenkins/jenkins_stack.py
+def test_sqs_queue_created():
+    app = core.App()
+    stack = JenkinsStack(app, "jenkins")
+    template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
